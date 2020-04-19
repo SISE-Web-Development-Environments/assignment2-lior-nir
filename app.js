613fbb8e518a8cd68c77111e61068780d960b3f7
Nir <nirdz@post.bgu.ac.il>
2020-04-19 18:15:47 +0300

    initial layout

--- app.js
+++ app.js
@@ -8,9 +8,40 @@ var time_elapsed;
 var interval;
 
 $(document).ready(function() {
-	context = canvas.getContext("2d");
-	Start();
+	goToWelcome();
+	$("#game").hide();
+//	context = canvas.getContext("2d");
+//	Start();
 });
+
+function goToWelcome(){
+	  $("#welcome").show();
+	  $("#login").hide();
+	  $("#about").hide();
+	  $("#register").hide();
+}
+
+function goToRegiser(){
+	$("#welcome").hide();
+	$("#login").hide();
+	$("#about").hide();
+	$("#register").show();
+}
+
+function goToLogin(){
+	$("#welcome").hide();
+	$("#login").show();
+	$("#about").hide();
+	$("#register").hide();
+}
+
+function goToAbout(){
+	$("#welcome").hide();
+	$("#login").hide();
+	$("#about").show();
+	$("#register").hide();
+}
+
 
 function Start() {
 	board = new Array();
